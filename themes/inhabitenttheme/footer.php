a1b2beafeb5a20810b622ac8af73b9abbacab8f3
Paul Petillot <petillotpaul@gmail.com>
2018-11-15 14:39:29 +0000

    First commit

---
+++ themes/inhabitenttheme/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+				 <img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-text-dark.svg" alt="the logo of the website">
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
