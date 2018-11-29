43466987b1468ee1abaa249f62bb3cd975883720
Paul Petillot <petillotpaul@gmail.com>
2018-11-29 14:07:06 +0000

    ES-Lint fixing

--- themes/inhabitenttheme/js/header-handler.js
+++ themes/inhabitenttheme/js/header-handler.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
-    if (window.location.pathname == '/Inhabitent/') {
-        var header = $("header");
+    const $header = $('header');
+    const $siteNav =  $('.site-nav');
+    if (window.location.pathname === '/Inhabitent/') {
         $('.site-nav').removeClass('site-header');
         $('.site-nav').addClass('site-header-nav');
         $(document).scroll(function() {    
-            var scroll = $(document).scrollTop();
-            if (scroll > $(window).height()) {
-            header.removeClass('site-header-nav').addClass("site-header");
+            let $scroll = $(document).scrollTop();
+            if ($scroll > $(window).height()) {
+            $header.removeClass('site-header-nav').addClass('site-header');
             } else {
-            header.removeClass("site-header").addClass('site-header-nav');
+            $header.removeClass('site-header').addClass('site-header-nav');
             }
            });
-    }else if(window.location.pathname == '/Inhabitent/about/'){
-        var header = $("#masthead");
+    }else if(window.location.pathname === '/Inhabitent/about/'){
+        $siteNav.removeClass('site-header');
+        $siteNav.addClass('site-header-nav');
         $(document).scroll(function() {    
-            var scroll = $(document).scrollTop();
-            if (scroll > $(window).height()) {
-            header.removeClass('site-header-nav').addClass("site-header");
+            let $scroll = $(document).scrollTop();
+            if ($scroll > $(window).height()) {
+                $siteNav.removeClass('site-header-nav').addClass('site-header');
             } else {
-            header.removeClass("site-header").addClass('site-header-nav');
+                $siteNav.removeClass('site-header').addClass('site-header-nav');
             }
            });
-        $('.site-nav').removeClass('site-header');
-        $('.site-nav').addClass('site-header-nav');
-}else if(window.location.pathname.includes("adventure/")){
-    var header = $("#masthead");
+        
+}else if(window.location.pathname.includes('/adventure/')){
     $(document).scroll(function() {    
-        var scroll = $(document).scrollTop();
-        if (scroll > $(window).height()) {
-        header.removeClass('site-header-nav').addClass("site-header");
+        let $scroll = $(document).scrollTop();
+        if ($scroll > $(window).height()) {
+            $siteNav.removeClass('site-header-nav').addClass('site-header');
         } else {
-        header.removeClass("site-header").addClass('site-header-nav');
+            $siteNav.removeClass('site-header').addClass('site-header-nav');
         }
        });
-    $('.site-nav').removeClass('site-header');
-    $('.site-nav').addClass('site-header-nav');
+       $siteNav.removeClass('site-header');
+       $siteNav.addClass('site-header-nav');
 }
 
 });
